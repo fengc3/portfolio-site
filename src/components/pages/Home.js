b098f5dbd128e4e7dc40d278f04d8a17eae08489
Carl <carlfeng3@gmail.com>
2025-02-06 22:16:20 -0500

    added cards and footer, footer needs css formatting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "../../App.css"
 import Hero from "../HeroSection"
+import Cards from "../Cards"
+import Footer from "../Footer"
 
 function Home () {
     return (
         <>
             <Hero />
+            <Cards />
+            <Footer />
         </>
     )
 }
