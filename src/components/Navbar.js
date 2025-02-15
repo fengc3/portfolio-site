d86e7362d71ad94af882fa78ed20b191c18f8248
Carl Feng <carlfeng3@gmail.com>
2025-02-15 18:19:55 -0500

    Navigation bar contact button tentatively removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import React, { useState, useEffect } from 'react'
 import "./Navbar.css"
-import { Button } from "./Button";
+// import { Button } from "./Button";
 
 function Navbar({jumpToSection}) {
     const [click, setClick] = useState(false);
